22462919c2f66bf3f9c48bb743a19241adb0b6e7
clarencethe3rd <clarencechan524@gmail.com>
2025-05-23 15:45:46 +0100

    Update memorizer.py

--- memorizer.py
+++ memorizer.py
@@ -1,25 +1,42 @@
 from tkinter import*
 from tkinter.ttk import*
+from tkinter.filedialog import*
 gui = Tk()
 
 gui.geometry("800x500")
 
 gui.title("Memorizer")
 
+def save():
+    f=asksaveasfile(defaultextension=".txt")
+    for i in listbox.get(0,END):
+        print(i.strip(),file=f)
+        
+def open_file():
+    f=askopenfile(title = "Open file")
+    content = f.readlines()
+    listbox.delete(0,END)
+    for i in content:
+        listbox.insert(END,i)
+def add():
+    e = entry.get()
+    listbox.insert(END,e)
+    entry.delete(0,END)
+
 def delete1():
     index = listbox.curselection()
     listbox.delete(index)
 
-save_button = Button(text="SAVE")
+save_button = Button(text="SAVE",command=save)
 save_button.pack(pady=(20,0))
 
 entry = Entry()
 entry.pack()
 
-add_button = Button(text="ADD")
+add_button = Button(text="ADD",command=add)
 add_button.pack()
 
-open_button = Button(text="OPEN")
+open_button = Button(text="OPEN",command=open_file)
 open_button.place(x=0,y=150)
 
 
